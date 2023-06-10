1434c35e46ef89ef0853232d6730f0b3a5b9e222
Andy Wood <andynwood@gmail.com>
2023-06-10 09:51:58 +0100

    cypress tests for search set up

--- cypress/e2e/integration/home.cy.js
+++ cypress/e2e/integration/home.cy.js
@@ -7,6 +7,44 @@ describe('Homepage', () => {
     cy.get('nav').should('exist')
   })
 
+  it('should have a series of links that lead to other pages', () => {
+    cy.get('a').each(link => {
+      cy.request(link.prop('href'))
+    })
+  })
+
+  // it('should go to the about page when the link is clicked', () => {
+  //   cy.get('nav').within(() => {
+  //     cy.get('a[href*="/about"]').last().click()
+  //     cy.get('div.slate-900').should('include', 'About')
+  //     cy.go('back')
+  //   })
+  // })
+
+  // it('should go to the search page when the link is clicked', () => {
+  //   cy.get('nav').within(() => {
+  //     cy.get('a[href*="/search"]').last().click()
+  //     cy.get('input').its('placeholder').should('eq', 'Search by value')
+  //     cy.go('back')
+  //   })
+  // })
+
+  // it('should go to the docs page when the link is clicked', () => {
+  //   cy.get('nav').within(() => {
+  //     cy.get('a[href*="/docs"]').last().click()
+  //     cy.get('div.slate-900').should('include', 'Docs')
+  //     cy.go('back')
+  //   })
+  // })
+
+  // it('should go to the login page when the link is clicked', () => {
+  //   cy.get('nav').within(() => {
+  //     cy.get('a[href*="/login"]').last().click()
+  //     cy.get('input').first().its('placeholder').should('eq', 'enter email')
+  //     cy.go('back')
+  //   })
+  // })
+
   it('should render a footer', () => {
     cy.get('footer').should('exist')
   })
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,172 +1,172 @@
-import { useState, useEffect, useContext } from "react";
-import React from "react";
+import { useState, useEffect, useContext } from 'react'
+import React from 'react'
 import {
-	Avatar,
-	Navbar,
-	MobileNav,
-	Typography,
-	Button,
-	IconButton
-} from "@material-tailwind/react";
+  Avatar,
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton
+} from '@material-tailwind/react'
 
-import UserContext from "../../context/user/UserContext";
+import UserContext from '../../context/user/UserContext'
 
-export default function Example() {
-	const UserSearchContext = useContext(UserContext);
+export default function Example () {
+  const UserSearchContext = useContext(UserContext)
 
-	const { user, avatar_url, username } = UserSearchContext;
+  const { user, avatar_url, username } = UserSearchContext
 
-	const [openNav, setOpenNav] = useState(false);
+  const [openNav, setOpenNav] = useState(false)
 
-	useEffect(() => {
-		window.addEventListener(
-			"resize",
-			() => window.innerWidth >= 960 && setOpenNav(false)
-		);
-	}, []);
+  useEffect(() => {
+    window.addEventListener(
+      'resize',
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    )
+  }, [])
 
-	const navList = (
-		<ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-			<Typography
-				as="li"
-				variant="small"
-				color="blue-gray"
-				className="p-1 font-normal"
-			>
-				<a href="/about" className="flex items-center">
-					About
-				</a>
-			</Typography>
-			<Typography
-				as="li"
-				variant="small"
-				color="blue-gray"
-				className="p-1 font-normal"
-			>
-				<a href="/search" className="flex items-center">
-					Search
-				</a>
-			</Typography>
-			<Typography
-				as="li"
-				variant="small"
-				color="blue-gray"
-				className="p-1 font-normal"
-			>
-				<a href="/signup" className="flex items-center">
-					SignUp
-				</a>
-			</Typography>
-			<Typography
-				as="li"
-				variant="small"
-				color="blue-gray"
-				className="p-1 font-normal"
-			>
-				<a href="docs" className="flex items-center mr-5">
-					Docs
-				</a>
-			</Typography>
-			<Typography
-				as="li"
-				variant="small"
-				color="blue-gray"
-				className="p-1 font-normal"
-			>
-				<span className="flex items-center mr-1">{username}</span>
-			</Typography>
-			{avatar_url && (
-				<Avatar
-					alt="avatar"
-					className="flex items-center mr-1 font-normal"
-					size="sm"
-					variant="circular"
-					color="blue-gray"
-					src={avatar_url}
-				/>
-			)}
+  const navList = (
+    <ul className='mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6'>
+      <Typography
+        as='li'
+        variant='small'
+        color='blue-gray'
+        className='p-1 font-normal'
+      >
+        <a href='/about' className='flex items-center'>
+          About
+        </a>
+      </Typography>
+      <Typography
+        as='li'
+        variant='small'
+        color='blue-gray'
+        className='p-1 font-normal'
+      >
+        <a href='/search' className='flex items-center'>
+          Search
+        </a>
+      </Typography>
+      <Typography
+        as='li'
+        variant='small'
+        color='blue-gray'
+        className='p-1 font-normal'
+      >
+        <a href='/signup' className='flex items-center'>
+          SignUp
+        </a>
+      </Typography>
+      <Typography
+        as='li'
+        variant='small'
+        color='blue-gray'
+        className='p-1 font-normal'
+      >
+        <a href='/docs' className='flex items-center mr-5'>
+          Docs
+        </a>
+      </Typography>
+      <Typography
+        as='li'
+        variant='small'
+        color='blue-gray'
+        className='p-1 font-normal'
+      >
+        <span className='flex items-center mr-1'>{username}</span>
+      </Typography>
+      {avatar_url && (
+        <Avatar
+          alt='avatar'
+          className='flex items-center mr-1 font-normal'
+          size='sm'
+          variant='circular'
+          color='blue-gray'
+          src={avatar_url}
+        />
+      )}
 
-			<Typography
-				as="li"
-				variant="small"
-				color="blue-gray"
-				className="p-1 font-normal"
-			>
-				{window.innerWidth >= 960 &&
-					(user ? (
-						<a href="logout" className="flex items-center mr-3">
-							Logout
-						</a>
-					) : (
-						<a href="login" className="flex items-center mr-3">
-							Login
-						</a>
-					))}
-			</Typography>
-		</ul>
-	);
+      <Typography
+        as='li'
+        variant='small'
+        color='blue-gray'
+        className='p-1 font-normal'
+      >
+        {window.innerWidth >= 960 &&
+          (user ? (
+            <a href='logout' className='flex items-center mr-3'>
+              Logout
+            </a>
+          ) : (
+            <a href='login' className='flex items-center mr-3'>
+              Login
+            </a>
+          ))}
+      </Typography>
+    </ul>
+  )
 
-	return (
-		<Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
-			<div className="container mx-auto flex items-center justify-between text-blue-gray-900">
-				<Typography
-					as="a"
-					href="/"
-					variant="small"
-					className="mr-4 cursor-pointer py-1.5 font-normal"
-				>
-					<span>Competency Recruitment Prepper</span>
-				</Typography>
-				<div className="hidden lg:block">{navList}</div>
-				<Button variant="gradient" size="sm" className="hidden lg:inline-block">
-					<a href="login">Login</a>
-				</Button>
-				<IconButton
-					variant="text"
-					className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-					ripple={false}
-					onClick={() => setOpenNav(!openNav)}
-				>
-					{openNav ? (
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							className="h-6 w-6"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-							strokeWidth={2}
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M6 18L18 6M6 6l12 12"
-							/>
-						</svg>
-					) : (
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="h-6 w-6"
-							fill="none"
-							stroke="currentColor"
-							strokeWidth={2}
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M4 6h16M4 12h16M4 18h16"
-							/>
-						</svg>
-					)}
-				</IconButton>
-			</div>
-			<MobileNav open={openNav}>
-				<div className="container mx-auto">
-					{navList}
-					<Button variant="gradient" size="sm" fullWidth className="mb-2">
-						{user ? <a href="logout">Logout</a> : <a href="login">Login</a>}
-					</Button>
-				</div>
-			</MobileNav>
-		</Navbar>
-	);
+  return (
+    <Navbar className='mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4'>
+      <div className='container mx-auto flex items-center justify-between text-blue-gray-900'>
+        <Typography
+          as='a'
+          href='/'
+          variant='small'
+          className='mr-4 cursor-pointer py-1.5 font-normal'
+        >
+          <span>Competency Recruitment Prepper</span>
+        </Typography>
+        <div className='hidden lg:block'>{navList}</div>
+        <Button variant='gradient' size='sm' className='hidden lg:inline-block'>
+          <a href='login'>Login</a>
+        </Button>
+        <IconButton
+          variant='text'
+          className='ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden'
+          ripple={false}
+          onClick={() => setOpenNav(!openNav)}
+        >
+          {openNav ? (
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              className='h-6 w-6'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M6 18L18 6M6 6l12 12'
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-6 w-6'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M4 6h16M4 12h16M4 18h16'
+              />
+            </svg>
+          )}
+        </IconButton>
+      </div>
+      <MobileNav open={openNav}>
+        <div className='container mx-auto'>
+          {navList}
+          <Button variant='gradient' size='sm' fullWidth className='mb-2'>
+            {user ? <a href='logout'>Logout</a> : <a href='login'>Login</a>}
+          </Button>
+        </div>
+      </MobileNav>
+    </Navbar>
+  )
 }
